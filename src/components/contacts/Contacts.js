5669a5dfe46a06d21862b193616a6d2494a3cca1
Mostafa El-Assar <mostafa.elassarg15@gmail.com>
2018-09-13 17:37:09 +0200

    Connect the contacts component with the contactReducer

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 import Contact from './Contact';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { GET_CONTACTS } from '../../actions/types';
 
 class Contacts extends Component {
+  componentDidMount() {
+    this.props.getContacts();
+  }
+
   render() {
-    const { contacts } = this.state;
+    const { contacts } = this.props;
     return (
       <React.Fragment>
         <h1 className="display-4 mb-2">
@@ -17,4 +24,20 @@ class Contacts extends Component {
   }
 }
 
-export default Contacts;
+Contacts.propTypes = {
+  contacts: PropTypes.array.isRequired,
+  getContacts: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  contacts: state.contact.contacts
+});
+
+const mapDispatchToProps = dispatch => ({
+  getContacts: () => dispatch({ type: GET_CONTACTS })
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Contacts);

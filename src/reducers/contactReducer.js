d57855c9f8523983499ed4bd264cd38a30241b2a
Mostafa El-Assar <mostafa.elassarg15@gmail.com>
2018-09-14 20:40:26 +0200

    Add the logic for editContact to contactReducer

--- src/reducers/contactReducer.js
+++ src/reducers/contactReducer.js
@@ -35,6 +35,14 @@ export default function(state = initialState, action) {
         ...state,
         contacts: [action.payload, ...state.contacts]
       };
+    case UPDATE_CONTACT:
+      return {
+        ...state,
+        contacts: state.contacts.map(
+          contact =>
+            contact.id === action.payload.id ? action.payload : contact
+        )
+      };
     default:
       return state;
   }
